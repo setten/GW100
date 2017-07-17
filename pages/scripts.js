487f0ae821c5c8dccddf1721a6aa668eb1f47342
Michiel van Setten <mjvansetten@gmail.com>
2017-07-17 09:12:39 +0200

    compare all

--- pages/scripts.js
+++ pages/scripts.js
@@ -268,28 +268,18 @@ function sortTable(table,n) {
   table = document.getElementById(table);
   switching = true;
   while (switching) {
-    //start by saying: no switching is done:
     switching = false;
     rows = table.getElementsByTagName("TR");
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
     for (i = 2; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
       shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
-      //check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
         shouldSwitch= true;
         break;
       }
     }
     if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
